54b9ae300aed186fadf551dc91ea7c3cfa22f5f5
Aleks <heoh888@ya.ru>
2022-06-02 19:15:39 +0300

    Initial Commit

---
+++ Millionaire/MillionaireApp.swift
@@ -0,0 +1,17 @@
+//
+//  MillionaireApp.swift
+//  Millionaire
+//
+//  Created by Алексей Ходаков on 02.06.2022.
+//
+
+import SwiftUI
+
+@main
+struct MillionaireApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
